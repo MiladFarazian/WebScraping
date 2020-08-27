7ef20c23e7586df132c1a50cedf81ae23d9135e0
Milad Farazian <mfarazia@ucsc.edu>
2020-08-26 22:00:15 -0700

    Edit functions

--- scrape.py
+++ scrape.py
@@ -27,11 +27,20 @@ def get_instagram_stats():
     followers = stats[2].text.strip()
     print("Instagram Stats: {} following and {} followers".format(following, followers))
 
-def get_facebook_stats():
-    pass
+""" def get_facebook_stats():
+    facebook_url = "https://www.facebook.com/miladfarazian/"
+    driver = webdriver.Chrome()
+    driver.get(facebook_url)
+    content = driver.page_source.encode('utf-8').strip()
+    facebook_soup = BeautifulSoup(content, "lxml")
+    stats = facebook_soup.findAll("span", class_ ="oi732d6d ik7dh3pa d2edcug0 qv66sw1b c1et5uql a8c37x1j muag1w35 enqfppq2 jq4qci2q a3bd9o3v lrazzd5p m9osqain")
+    for stat in stats:
+        print(stat.text.strip()) """
+
 
 def get_social_media_stats():
     pass
 
-get_twitter_stats()
-get_instagram_stats()
+#get_twitter_stats()
+#get_instagram_stats()
+#get_facebook_stats()
